8ef02d373cc22f96d26a25248b056340c6208116
Tyler Hunt <tyhunt04@gmail.com>
2019-03-14 17:58:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,41 +1,48 @@
-INSERT INTO users (name, age) VALUES ("Dave", 25);
-INSERT INTO users (name, age) VALUES ("Sam", 31);
-INSERT INTO users (name, age) VALUES ("Frank", 21);
-INSERT INTO users (name, age) VALUES ("Joe", 26);
-INSERT INTO users (name, age) VALUES ("Ian", 46);
-INSERT INTO users (name, age) VALUES ("Anthony", 35);
-INSERT INTO users (name, age) VALUES ("Tyler", 19);
-INSERT INTO users (name, age) VALUES ("Eli", 25);
-INSERT INTO users (name, age) VALUES ("Steve", 29);
-INSERT INTO users (name, age) VALUES ("Dan", 34);
-INSERT INTO users (name, age) VALUES ("Sara", 36);
-INSERT INTO users (name, age) VALUES ("Kasey", 24);
-INSERT INTO users (name, age) VALUES ("Alisha", 39);
-INSERT INTO users (name, age) VALUES ("Kristi", 45);
-INSERT INTO users (name, age) VALUES ("Don", 42);
-INSERT INTO users (name, age) VALUES ("Toni", 35);
-INSERT INTO users (name, age) VALUES ("Tanya", 27);
-INSERT INTO users (name, age) VALUES ("Bob", 26);
-INSERT INTO users (name, age) VALUES ("Gary", 38);
-INSERT INTO users (name, age) VALUES ("Donna", 28);
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
- VALUES ("The Best", "Business", 500000, "3\10\19", "3\10\21");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("The new Baseball", "Sports", 20000, "7\21\19", "6\25\20");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Night Vision Glasses", "Tech", 1000000, "4\2\19", "9\8\20");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Insta Omelet", "Food", 5000, "5\15\19", "10\10\19");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Forever Fireplace", "Business", 500000, "11\26\19", "12\25\21");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Dander Free Life", "Business",10000, "1\1\19", "1\1\20");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("VR Office", "Tech", 1000000, "2\14\19", "7\4\22");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Perfect Swing Golf Club", "Sports", 500000, "1\31\19", "12\31\20");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("80 Proof Beer", "Food", 500000, "8\1\18", "8\31\20");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-  VALUES ("Self Driving Bike", "Tech", 15000, "3\10\19", "3\10\21");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
